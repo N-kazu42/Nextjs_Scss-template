5a94800f8cbc1724fdac6d2c358ac69181d5a6a2
N-kazu42 <shioatsupapakazuaki@gmail.com>
2022-07-04 09:05:13 +0900

    animate追加　HamburgMenu 追加

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,9 +1,40 @@
 import Layout from "../components/Layout";
+import {motion} from "framer-motion";
+import styles from "../styles/Home.module.scss";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useAnimation } from "framer-motion";
 
-const Contact= () => {
+const Contact = () => {
+  const {ref, inView } = useInView({
+    threshold:0.2
+  });
+  const animation = useAnimation();
+
+  useEffect(() =>{
+    console.log("use effect hook, inView =", inView);
+    if(inView){
+      animation.start({
+        x: 0,
+        transition: {
+          type: 'spring',duration: 1, bounce: 0.3
+        }
+      });
+    }
+    if(!inView){
+      animation.start({x:'-100vh'})
+    }
+    
+  },[inView]);
   return (
     <Layout title="Contact">
-    <div>contact-page</div>
+      <div className={styles.box}>testcontainer</div>
+      <motion.div 
+      animate={animation} >
+      <div ref={ref}>
+        <p className={styles.box2}>contact-page</p>
+      </div>
+      </motion.div>
     </Layout>
   )
 }
